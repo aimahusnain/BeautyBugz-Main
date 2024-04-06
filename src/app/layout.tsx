cab2c224515a859e0588944945763efacca2c137
aimahusnain <aimahusnain@gmail.com>
2024-04-07 02:57:08 +0500

    Hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.$crisp=[];
+          window.CRISP_WEBSITE_ID="efd5f37b-eea3-40ee-9cfe-b489ec358910";
+        (function(){
+          var d = document;
+         var  s=d.createElement("script");
+          s.src="https://client.crisp.chat/l.js";
+          s.async=1;
+          d.getElementsByTagName("head")[0].appendChild(s);})();
+            `,
+          }}
+        ></script>
+      </head>
       <body
         // bg-desired-white
         className={`${inter.className} bg-pink-500 text-your-desired-text-color`}
       >
         {/* <Sale /> */}
         {/* <Header /> */}
+
         <SessionProvider>{children}</SessionProvider>
         {/* <UpiUp /> */}
         {/* <Footer /> */}
